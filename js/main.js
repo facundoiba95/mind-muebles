4eb5a628d23e8b90c82c5fe85bd9eb1accf346c0
facundoiba95 <facundoiba95@gmail.com>
2022-09-08 13:14:09 -0300

    cambio en imagenes, y agregue logica del search en main.js

--- js/main.js
+++ js/main.js
@@ -1,37 +1,122 @@
-// CREA UN ARRAY DE MUEBLES
-//Propiedades: id, nombre, categoria, color,
-
 let Muebles = [
     {
         id:1,
-        nombre: 'bajo mesada',
+        nombre: 'Bajo mesada',
         categoria: 'cocina',
-        color: ['marron','negro','marron claro']
+        color: ['marron','negro','marron claro'],
+        tags: ['mesada','mueble para cocina','bajo mesada','cocina']
+
     },
     {
         id:2,
-        nombre: 'sillon dos partes',
+        nombre: 'Sillon dos partes',
         categoria: 'living',
-        color: ['gris','negro','marron claro']
+        color: ['gris','negro','marron claro'],
+        tags: ['sillon','living','dos cuerpos','sofa','negro']
     },
     {
         id:3,
-        nombre: 'escritorio',
+        nombre: 'Escritorio',
         categoria: 'oficina',
-        color: ['marron','negro','blanco']
+        color: ['marron','negro','blanco'],
+        tags: ['escritorio','mesa','oficina','office','negro','escritorio de oficina','pc','para pc','oficina']
+    },
+    {
+        id:4,
+        nombre: 'Mesa',
+        categoria: 'living',
+        color: ['marron','negro','blanco'],
+        tags: ['escritorio','living','mesa','mesa de living']
+    },
+    {
+        id:5,
+        nombre: 'Castillo para niños',
+        categoria: 'dormitorio',
+        color: ['blanco','marron','madera'],
+        tags: ['cama','dormitorio','niños','castillo','para niños','muebles para niños']
     }
-]
 
 
-function capturarDatosBusqueda (){
-    var datosBusqueda = document.getElementById("inputsearch").value;
-   return datosBusqueda;
+];
+
+//traigo elementos del html
+const searchBtn = document.querySelector('.btnSearch')
+const inputSearch = document.querySelector('#search')
+const productosFiltrados = document.querySelector('.productosFiltrados')
+const messageSearch = document.querySelector('.message-search')
+
+//nuevo array de Muebles para guardar los filtrados;
+let nuevoMuebles = [];
+
+
+
+//funcion para crear el Html
+const createHtmlMuebles = muebles => {
+    return `
+    <li class="productosFiltrados-item">
+        <h3 class="titulo-search">${muebles.nombre}</h3>
+        <h4 class="categoria-search">${muebles.categoria}</h4>
+        <h4 class="colores-search">${muebles.color.join(', ')}</h4>
+    </li>`
 }
-function mostrarBusqueda(){
-    let nuevoArray=[];
 
-   nuevoArray.push(capturarDatosBusqueda())
-      
-    return nuevoArray;
+//funcion para renderizar el Javascript en el Html
+const renderMuebles = muebles => {
+    productosFiltrados.innerHTML = muebles.map(mueble => createHtmlMuebles(mueble))
 }
+
+
+//funcion para buscar un producto
+const searchProduct = e => {
+    e.preventDefault();
+    
+    const nameProduct = inputSearch.value.trim();
+
+    if(!nameProduct.length){
+        messageSearch.textContent = 'Por favor, ingresa un producto!';
+        inputSearch.style.border = '3px solid red';
+        nuevoMuebles=[];
+        renderMuebles(nuevoMuebles);
+        
+    }else if(
+        //filtro elementos que los tags coincidan
+        (nuevoMuebles = Muebles.filter(mueble => mueble.tags?.includes(nameProduct)) == 0)
+    ){
+        messageSearch.textContent='Ups! No se encontro ese producto !';
+        inputSearch.style.border = '3px solid gray'
+        nuevoMuebles = [];
+        renderMuebles(nuevoMuebles);
+        return;
+    }else if(
+        // ME QUEDE EN ESTA FUNCION!  funciona cuando si coinciden, pero no muestra el mensaje al no coincidir!
+        nuevoMuebles = Muebles.filter(mueble => mueble.tags?.includes(nameProduct))
+    ){
+        messageSearch.textContent = '';
+        inputSearch.style.border= '3px solid rgb(146, 0, 209)'
+        renderMuebles(nuevoMuebles);
+        return;
+    }
+  
+
+    renderMuebles(nuevoMuebles)
+
+}
+
+
+const init = () => {
+    searchBtn.addEventListener('click', searchProduct)
+}
+init();
+
+
+
+
+
+
+
+
+
+
+
+
 
