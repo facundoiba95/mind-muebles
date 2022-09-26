b057fa2a7b1ccaf950cf12f71b6f44df9387d5c9
facundoiba95 <facundoiba95@gmail.com>
2022-09-26 20:02:15 -0300

    commit random

--- js/productosHtml.js
+++ js/productosHtml.js
@@ -1,13 +1,4 @@
-const searchAdvancedLink = document.querySelector('.search-advanced-link')
-const searchAdvanced = document.querySelector('.search-advanced-container')
-searchAdvanced.style.display = "none"
-const productosFiltradosItem = document.querySelector('.productosFiltrados-item')
 
-const mostrarProducto = e => {
-     e.target.style.border ='4px solid red';
-
-    // Muebles.map(producto => producto.id == productoFiltrado ? alert('hola') : alert('chau'))
-}
 
 
 
